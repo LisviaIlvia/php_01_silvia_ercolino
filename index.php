1f2df36e99a057b6f04987862c4fe42d72b61a10
silvi <silviaercolino@yahoo.it>
2024-09-05 21:42:10 +0200

    aggiunto commenti

--- index.php
+++ index.php
@@ -2,21 +2,25 @@
 
 // Selfwork PHP 1
 
+// Dichiarazione delle variabili
 $integer = 10;
 $float = 35.48;
 $string = "Silvia";
 $boolean = true;
 
+// Visualizza il tipo di dato e il valore delle variabili
 var_dump($integer);
 var_dump($float);
 var_dump($string);
 var_dump($boolean);
 
+// Trasformazione delle variabili in costanti
 define('CONST_INTEGER', $integer);
 define('CONST_FLOAT', $float);
 define('CONST_STRING', $string);
 define('CONST_BOOLEAN', $boolean);
 
+// Visualizza il tipo di dato e il valore delle costanti
 var_dump(CONST_INTEGER);
 var_dump(CONST_FLOAT);
 var_dump(CONST_STRING);
